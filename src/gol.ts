9e73cc21238acbea4961fae21c7ffca5ae46d46d
Nick Kreeger <nick.kreeger@gmail.com>
2017-11-29 08:31:50 -0800

    wip

---
+++ src/gol.ts
@@ -0,0 +1,14 @@
+import {Array2D} from 'deeplearn';
+
+function generateGolExample(size: number) {
+  if (size < 3) {
+    new Error('Size must be greater than 2');
+  }
+
+  const world = Array2D.randUniform([size - 2, size -2], 0, 2, 'int32');
+  console.log('world', world.getValues());
+}
+
+
+// Start:
+generateGolExample(3);
